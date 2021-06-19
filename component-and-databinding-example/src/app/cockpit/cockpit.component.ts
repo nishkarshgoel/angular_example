fd0277664ef59f27de92ea03eeb7f3cdb72b636e
nishkarshgoel <goel.nix@gmail.com>
2021-06-19 17:00:08 +0530

    added code examples related to Components and Databinding in detail

---
+++ component-and-databinding-example/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  //newServerName = '';
+  //newServerContent = '';
+  @ViewChild('serverContentInput') serverContInput: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer(serverNameInput: HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName: serverNameInput.value,
+      serverContent: this.serverContInput.nativeElement.value
+    });
+  }
+
+  onAddBlueprint(serverNameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: serverNameInput.value,
+      serverContent: this.serverContInput.nativeElement.value
+    });
+  }
+}
